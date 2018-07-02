ff5685cb7c1278356e083d683f1e66a57b6b0a2a
vocationbliss <38209106+vocationbliss@users.noreply.github.com>
2018-07-02 14:18:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {myFunction()};
+
+var header = header.offsetTop;
+
+function myFunction() {
+if (window.pageYOffset >= sticky)}
+ header.classList.add("sticky");
+} else {
+ header.classList.remove("sticky");
+}
+}
